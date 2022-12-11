6bb2be11b875af32bcae47fb151157763882abbd
vijeyash1 <vijeyash@gmail.com>
2022-12-11 13:53:08 +0530

    envconfig

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -6,16 +6,32 @@ import (
 	"log"
 
 	"github.com/google/uuid"
+	"github.com/kelseyhightower/envconfig"
 	"github.com/vijeyash1/backend/pkg/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
+type Db struct {
+	Host     string `envconfig:"host" default:"localhost" required:"false"`
+	Database string `envconfig:"db" default:"core" required:"false"`
+	Port     int    `envconfig:"dbport" default:"5432" required:"false"`
+	User     string `envconfig:"dbuser" default:"core" required:"false"`
+	Password string `envconfig:"dbpassword" default:"core" required:"false"`
+	Ssl      string `envconfig:"sslmode" default:"disable" required:"false"`
+}
+
 func Init() {
 	log.Println("Initializing database and tables...")
 	log.Println("Initializing database with sample data...")
-
-	dbURI := "host=localhost user=core password=core dbname=core port=5432 sslmode=disable"
+	dbmetadata := &Db{}
+	err := envconfig.Process("", dbmetadata)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	format := "host=%s user=%s password=%s dbname=%s port=%d sslmode=%v"
+	dbURI := fmt.Sprintf(format, dbmetadata.Host, dbmetadata.User, dbmetadata.Password, dbmetadata.Database, dbmetadata.Port, dbmetadata.Ssl)
+	// dbURI := "host=localhost user=core password=core dbname=core port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dbURI), &gorm.Config{})
 	if err != nil {
 		log.Fatal("unable to open database connection: ", err)
